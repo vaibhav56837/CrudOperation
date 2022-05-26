e802ea15e8d6759ba73de852981bd7bdafc4d3b2
vaibhav  samrit <vaibhavsamrit1@gmail.com>
2022-05-26 18:05:35 +0530

    MVC-CrudOpeartion Project completed

---
+++ CRUDOperationASPDotNETMVCEFCore/Migrations/20220526121128_initailMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDOperationASPDotNETMVCEFCore.Migrations
+{
+    public partial class initailMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Skills = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    TotalStudents = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<decimal>(type: "money", nullable: false),
+                    AddedOn = table.Column<DateTime>(type: "date", nullable: false, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
---
+++ CRUDOperationASPDotNETMVCEFCore/Models/Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDOperationASPDotNETMVCEFCore.Models
+{
+    public class Teacher
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Skills { get; set; }
+
+        [Range(5, 50)]
+        public int TotalStudents { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+
+        [Required]
+        public DateTime AddedOn { get; set; }
+
+
+    }
+}
